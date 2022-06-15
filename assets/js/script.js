261d3c53f0263b68c3e165a63ea26a0caa2599e8
Ryan Raposo <raposo.ryan@gmail.com>
2022-06-15 11:48:05 -0400

    add data parsing and setting of main info panel

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,51 @@ async function getWeather(cityName) {
     let data = await response.json();
     let weather = [];
     for (let x = 0; x < 5; x++) {
-        let daily = Object.fromEntries(['datetime', 'temp', 'windspeed', 'uvindex', 'humidity'].map((k) => [k, data.days[x][k]]));
-        weather.push(daily);   
+        const daily = data.days[x];
+        let newDaily = {
+            city: data.address,
+            temp: daily.temp,
+            humidity: daily.humidity,
+            uvindex: daily.uvindex,
+            wind: daily.windspeed
+        };
+        weather.push(newDaily);
     }
     return weather;
 }
 
-getWeather("toronto").then((result) => {console.log(result)});
+function applyWeather(weather) {
+    // Set Info Panel 
+    const hInfoPanelCity = document.querySelector('#h-info-panel-city');
+    hInfoPanelCity.innerText = "City: " + weather[0].city;
+
+    const pInfoPanelTemp = document.querySelector('#p-info-panel-temp');
+    pInfoPanelTemp.innerText = "Temp: " + weather[0].temp;
+
+    const pInfoPanelHumidity = document.querySelector('#p-info-panel-humidity');
+    pInfoPanelHumidity.innerText = "Humidity: " + weather[0].humidity;
+
+    const pInfoPanelUVIndex = document.querySelector('#p-info-panel-UV-Index')
+    pInfoPanelUVIndex.innerText = "UV-Index: " + weather[0].uvindex;
+}
+
+function searchCity(input) {
+    if (input) {
+        let weather = getWeather(input).then(weather => {
+            applyWeather(weather);
+        });
+    }
+}
+
+$('#btn-search').click(() => {
+    const value = $('#input-search').val().trim();
+    searchCity(value);
+});
+
+$('#input-search').keypress((e) => {
+    if ( e.which == 13 ) {
+        e.preventDefault();
+        const value = $('#input-search').val().trim();
+        searchCity(value);
+    }
+});
